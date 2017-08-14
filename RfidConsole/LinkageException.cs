f5bf96ab5a04e8ff7b44075d533d2f4e638d1e73
Stuart Hallows <shallows@somarkinnovations.com>
2017-08-14 16:20:02 +0930

    add async Main

---
+++ RfidConsole/LinkageException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RfidConsole
+{
+    public class LinkageException : Exception
+    {
+        public LinkageException()
+        {
+        }
+
+        public LinkageException(string message)
+            : base(message)
+        {
+        }
+
+        public LinkageException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
